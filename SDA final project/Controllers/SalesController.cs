0addb45af2818bca7378ee9afb14ae5a5af02524
Shaheen <engra@DESKTOP-LB3J34O>
2020-06-24 01:38:02 +0500

    sale report added

---
+++ SDA final project/Controllers/SalesController.cs
@@ -0,0 +1,29 @@
+﻿using SDA_final_project.View_Modals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace SDA_final_project.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        public ActionResult Sales()
+        {
+
+            if (WebSecurity.IsAuthenticated)
+            {
+
+                    ViewBag.sales = "active";
+                    return View(Miscellaneous.HabibDataClass.Habib.CustomerOrders.ToList());
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+    }
+}
