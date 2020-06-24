1c39fe6008da33510139f00afd0e295e79145fae
Shaheen <engra@DESKTOP-LB3J34O>
2020-06-24 12:06:50 +0500

    report detail module added

---
+++ SDA final project/View_Modals/SalesDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SDA_final_project.View_Modals
+{
+    public class SalesDetailsViewModel
+    {
+        public List<Tuple<string, string, string, string, string, int>> listdetails = new List<Tuple<string, string, string, string, string, int>>();
+    }
+}
